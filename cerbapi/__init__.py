ddce47518c0c68f26870a6efbfdbe63d871e35d6
Rob Adkerson <r.j.adkerson@gmail.com>
2018-05-08 15:30:09 -0400

    1.0.9 - Fixed bug in email parse

--- cerbapi/__init__.py
+++ cerbapi/__init__.py
@@ -4,7 +4,7 @@ from email.utils import formatdate
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, urlparse
 
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 
 
 class CerbException(Exception):
